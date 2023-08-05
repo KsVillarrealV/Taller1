cf9336380af50e41aeac89495ea9074a6230a551
KsVillarreal <kevinsvillareal@gmail.com>
2023-08-04 23:00:41 -0500

    corrección

--- src/Punto13.java
+++ src/Punto13.java
@@ -3,9 +3,8 @@ import javax.swing.JOptionPane;
 public class Punto13 {
     public static void main(String[] args) {
             
-            double decimal = Double.parseDouble(JOptionPane.showInputDialog("Ingrese un digito decimal: "));
-            
+            double decimal = Double.parseDouble(JOptionPane.showInputDialog("Ingrese un digito decimal con barios decimales: "));
             double roundOff = Math.round(decimal * 1000.0) / 1000.0;
-            JOptionPane.showMessageDialog(null,"Decimal aproximado: "+roundOff);
+            JOptionPane.showMessageDialog(null,"Decimal aproximado aproximado a tres decimales: "+roundOff);
     }
 }
